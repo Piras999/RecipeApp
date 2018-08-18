253a0eeacb69fdcd2e8dcd5abcf26e360270dbd7
Piotr Pirański <piotr.piranski@gmail.com>
2018-08-18 11:04:04 +0200

    First Steps

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+export default class Master extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>Heloo!!</div>
+        )
+    }
+}
